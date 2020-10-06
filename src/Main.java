27a19a61eb0a3eddb5b2569673cef06de899dea1
TaniS <t6346616@gmail.com>
2020-10-06 19:09:53 +0300

    Lesson 1

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println();
+        System.out.println("How I spent my summer");
+        System.out.println();
+        System.out.println("\t My summer vacations this year were interesting and memorable. Every day was filled with events." +
+                "\n" +
+                "In June, together with classmates, we went to a camp. I liked the camp very much." +
+                "\n" + "We played sports games, competed in various sports, took part in various competitions." +
+                "\n" + "In the evenings, we went to the disco. I enjoyed discos as I like music and dancing. " +
+                "\n" + "I made a lot of new friends and got to know my classmates better than at school.");
+
+    }
+}
